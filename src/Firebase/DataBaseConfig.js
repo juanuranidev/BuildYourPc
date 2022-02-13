3eaa7416f01668aaa46ef2f56c7ab33c36685f1f
Juan Urani <juanuranidev@gmail.com>
2022-02-13 19:55:51 -0300

    updates

---
+++ src/Firebase/DataBaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ1CGA7DUKKUib9fHrfX8vS7AcHR3nBwA",
+  authDomain: "buildyourpc-93d11.firebaseapp.com",
+  projectId: "buildyourpc-93d11",
+  storageBucket: "buildyourpc-93d11.appspot.com",
+  messagingSenderId: "665376515800",
+  appId: "1:665376515800:web:0344605906e1fd06af99ad"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const getFirestoreApp = () => {
+    return app 
+}
