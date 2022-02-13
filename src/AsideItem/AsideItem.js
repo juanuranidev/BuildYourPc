42900d6492322e77789d1446be14db20d7a6939d
Juan Urani <juanuranidev@gmail.com>
2022-02-13 20:18:18 -0300

    updates

--- src/AsideItem/AsideItem.js
+++ src/AsideItem/AsideItem.js
@@ -8,13 +8,13 @@ const AsideItem = (props) => {
  const { order } = useOrderContext()
 
   return (
-    <>
+    <div className='asideItem'>
     {
       order.find(e => e.category === props.category)
       ? <AsideProduct fetch={props.category} />
       : <AsideMessage img={props.img} h3={props.h3} p={props.p} category={props.category} />
     }    
-    </>
+    </div>
   )
 }
 
