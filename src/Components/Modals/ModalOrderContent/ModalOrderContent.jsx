42434150da34eef20f1d524ff319d37ffc3f14ee
Juan Urani <juanuranidev@gmail.com>
2022-10-25 00:53:46 -0300

    Working on ModalOrderContent

--- src/Components/Modals/ModalOrderContent/ModalOrderContent.jsx
+++ src/Components/Modals/ModalOrderContent/ModalOrderContent.jsx
@@ -3,19 +3,25 @@ import Products from '../../Products/Products'
 import "./ModalOrderContent.scss"
 import OrderProduct from './OrderProduct/OrderProduct'
 
-export default function ModalOrderContent({order, close, openPdf}) {
+export default function ModalOrderContent({order, close, openPdf, totalPrice}) {
   return (
-    <div className='modalOrderConten'>
-        <div className='modalOrderConten_header'>
-            <h2 className='modalOrderConten_header_h2'>Resumen de tu Pedido</h2>
-            <div className='modalOrderConten_header_div'>
-                <button className='modalOrderConten_header_div_button' onClick={openPdf}>Descargar PDF</button>
-                <p className='modalOrderConten_header_div_p' onClick={close}>Regresar y Editar</p>
-            </div>
+    <div className='modalOrderContent'>
+      <div className="modalOrderContent_close">
+        <span class="material-symbols-outlined modalOrderContent_close_p" onClick={close}>close</span>
+      </div>
+      <div className='modalOrderContent_header'>
+        <div className='modalOrderContent_header_main'>
+          <h2 className='modalOrderContent_header_main_h2'>Resumen de tu Pedido</h2>
+          <h4>Total del pedido: ${(totalPrice).toLocaleString("ES-ar")}</h4>
         </div>
-        <div className='modalOrderConten_content'>
-          {order.map((product) => <OrderProduct product={product} key={product.id} /> )}
+        <div className='modalOrderContent_header_div'>
+          <button className='modalOrderContent_header_div_button' onClick={openPdf}>Descargar PDF</button>
+          <p className='modalOrderContent_header_div_p' onClick={close}>Regresar y Editar</p>
         </div>
+      </div>
+      <div className='modalOrderContent_content'>
+        {order.map((product) => <OrderProduct product={product} key={product.id} /> )}
+      </div>
     </div>
   )
 }
